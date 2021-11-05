d4fe59a755b45e9cc18ba4124fdbc8b7797ba069
NEUdeep <hdkustl@163.com>
2021-11-05 10:59:40 +0800

    modified run/test.sh for get train map

--- run/test.sh
+++ run/test.sh
@@ -1,1 +1,5 @@
 ./tools/dist_test.sh ./detectors_cascade_rcnn_r50_1x_coco_B.py work_dirs/detectors_cascade_rcnn_r50_1x_coco/epoch_12.pth 2 --format-only --options "jsonfile_prefix=work_dirs/detectors_cascade_rcnn_r50_1x_coco/openBrand_result_12"
+
+./tools/dist_test.sh ./detectors_cascade_rcnn_r50_1x_coco_A.py work_dirs/detectors_cascade_rcnn_r50_1x_coco/epoch_150.pth 1 --format-only --options "jsonfile_prefix=work_dirs/detectors_cascade_rcnn_r50_1x_coco/openBrand_result_150"
+
+./tools/dist_test.sh ./detectors_cascade_rcnn_r50_1x_coco_A.py work_dirs/detectors_cascade_rcnn_r50_1x_coco/epoch_150.pth 1 --eval bbox #用于评估检测map; allowed_metrics = ['bbox', 'segm', 'proposal', 'proposal_fast']

4bb7f03336edb11b275d229ca6a831535666c902
NEUdeep <hdkustl@163.com>
2021-12-08 11:32:46 +0800

    add infer-tools

--- tools/demo.py
+++ tools/demo.py
@@ -1,24 +1,35 @@
 import mmcv
 import os
+import cv2
+from mmcv import Timer
+import time
 import numpy as np
 from mmcv.runner import load_checkpoint
 from mmdet.models import build_detector
 from mmdet.apis import init_detector, inference_detector
 
 
-config_file = 'detectors_cascade_rcnn_r50_1x_coco_A.py'
-checkpoint_file = 'work_dirs/detectors_cascade_rcnn_r50_1x_coco/epoch_12.pth'
+config_file = 'detectors_cascade_rcnn_r50_1x_coco_C.py'
+checkpoint_file = 'best/C_epoch_12.pth'
 
 model = init_detector(config_file,checkpoint_file)
 
-img_dir = '/root/public/Datasets/2021-industry-quality-inspection-competition/test/'
+img_dir = '/root/neu-lab/'
 out_dir = 'results/'
+test= 'test'
 
 if not os.path.exists(out_dir):
     os.mkdir(out_dir)
 
-img =img_dir + '004931.jpeg'
+img =img_dir + '001107.jpeg'
+s = time.time()
+infer_time = Timer()
 result = inference_detector(model,img)
-model.show_result(img, result, model.CLASSES)
+e = time.time()
+average_time = infer_time.since_start()
+# model.show_result(img, result)
+img = model.show_result(img, result)
+cv2.imwrite("{}/{}.jpg".format(out_dir, test), img)
 
-print(result)
+print("Using python time is:",e-s, "s")
+print("Using mmcv is:",average_time, "s")
